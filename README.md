e9128755c753f319902de1cb5492ddbc541a42a9
Milton Black <arziblack2@gmail.com>
2024-06-21 10:22:29 +0100

    feat: navbar in progress...

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## BLACKDICE SHOPPING ONLINE MART
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- app/components/Landing/Navbar.tsx
+++ app/components/Landing/Navbar.tsx
@@ -5,7 +5,7 @@ import { FaCartShopping, FaUser } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <div className="bg-[#003A74] w-full flex items-center pt-7 flex-col max-w-[1480px] min-w-[1366px]">
+    <div className="bg-[#003A74] w-full flex items-center pt-4 flex-col max-w-[1480px] min-w-[1366px]">
       <nav className="px-5 grid grid-cols-4 gap-4 p-4 w-full items-center">
         {/* The Logo Header, Section 1 */}
         <h2 className="col-span-1 text-left text-[24px] font-bold text-white">BLACKDICE</h2>
@@ -13,14 +13,14 @@ const Navbar = () => {
         <div className="col-span-3 flex items-center justify-between w-full">
           {/* The Search Bar */}
           <div className="flex items-center justify-between w-full">
-            <div className="flex items-center">
+            <div className="items-center grid grid-cols-3 gap-2">
               {/* The Input Bar */}
-              <div className="flex items-center p-2 border border-[#65A4E2] rounded max-w-[540px] w-full">
+              <div className="flex items-center p-2 border border-[#65A4E2] rounded max-w-[540px] w-full col-span-2">
                 <FaSearch />
                 <input placeholder="search here..." className="border-none bg-inherit outline-none ml-2 w-full flex" />
               </div>
               {/* The search button */}
-              <button className="bg-[#55A4B2] p-2">Search</button>
+              <button className="bg-[#55A4B2] p-2 col-span-1 rounded">Search</button>
             </div>
           </div>
           <div className="grid grid-cols-3 gap-3">
