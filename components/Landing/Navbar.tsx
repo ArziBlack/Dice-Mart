43cb3b79d57e4c0c3a41872b6a2be22f8a961b49
Milton Black <arziblack2@gmail.com>
2024-06-21 01:52:23 +0100

    ..

--- components/Landing/Navbar.tsx
+++ components/Landing/Navbar.tsx
@@ -3,8 +3,10 @@ import SubNavbar from './SubNavbar'
 
 const Navbar = () => {
   return (
-    <div>
-        <nav className="nav"></nav>
+    <div className='bg-[#003A74] w-full flex items-center px-5 py-7 flex-col'>
+        <nav className="">
+            <h2>BLACKDICE</h2>
+        </nav>
         <SubNavbar/>
     </div>
   )
