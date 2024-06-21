ad53d9ac09cf61027303a985c54c93e5ee6e2570
Milton Black <arziblack2@gmail.com>
2024-06-21 10:04:29 +0100

    feat: added layout to the main navbar...

--- app/components/Landing/Navbar.tsx
+++ app/components/Landing/Navbar.tsx
@@ -6,21 +6,21 @@ import { FaCartShopping, FaUser } from "react-icons/fa6";
 const Navbar = () => {
   return (
     <div className="bg-[#003A74] w-full flex items-center pt-7 flex-col max-w-[1480px] min-w-[1366px]">
-      <nav className="px-5 grid grid-cols-4 gap-4 p-4 w-full">
+      <nav className="px-5 grid grid-cols-4 gap-4 p-4 w-full items-center">
         {/* The Logo Header, Section 1 */}
-        <h2 className="col-span-1 text-left">BLACKDICE</h2>
+        <h2 className="col-span-1 text-left text-[24px] font-bold text-white">BLACKDICE</h2>
         {/* Section 2 */}
         <div className="col-span-3 flex items-center justify-between w-full">
           {/* The Search Bar */}
           <div className="flex items-center justify-between w-full">
             <div className="flex items-center">
               {/* The Input Bar */}
-              <div className="flex items-center">
+              <div className="flex items-center p-2 border border-[#65A4E2] rounded max-w-[540px] w-full">
                 <FaSearch />
-                <input placeholder="search here..." />
+                <input placeholder="search here..." className="border-none bg-inherit outline-none ml-2 w-full flex" />
               </div>
               {/* The search button */}
-              <button>Search</button>
+              <button className="bg-[#55A4B2] p-2">Search</button>
             </div>
           </div>
           <div className="grid grid-cols-3 gap-3">
