87125307d3442f7ace3d1038a65c04dc8b74fdf1
Milton Black <arziblack2@gmail.com>
2024-06-27 09:56:14 +0100

    fix: refactored itemcard...

--- app/components/Landing/ItemCard.tsx
+++ app/components/Landing/ItemCard.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { StaticImageData } from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import React from 'react'
 
 type ItemProps = {
@@ -9,23 +9,27 @@ type ItemProps = {
 }
 const ItemCard = ({ image, item, price}: ItemProps) => {
   return (
-    <div className="bg-white flex rounded-md w-full">
-          <div className="h-28 rounded-md bg-[#55A4B2] w-1/3">
-            <img
-              src={image as string}
-              className="w-28 h-28 rounded-md"
-              alt="product"
-            />
+    <div className="slide-one bg-white rounded-md w-[240px] flex flex-col items-start justify-between h-[350px]">
+      <div className="flex flex-col items-start justify-between">
+        <Image
+          src={image as string}
+          alt="generator"
+          className="w-full h-[220px] object-cover rounded-md"
+        />
+        <div className="flex flex-col items-start h-full px-2 py-4 text-sm w-full">
+          <h2 className="font-[800] py-1">{item}</h2>
+          <div className="flex w-full justify-between items-center">
+            <span className="font-[800]">${price}</span>
+            <span className="p-1 bg-stone-500 px-3 rounded-2xl text-white">
+              -24%
+            </span>
           </div>
-          <div className="flex flex-col justify-between h-full px-2 py-4 w-2/3 text-sm">
-            <h2 className="font-[800]">{item}</h2>
-            <div className="flex w-full justify-between items-center">
-              <span className="font-[800]">${price}</span>
-              <span className="p-1 bg-stone-500 px-3 rounded-2xl text-white">-24%</span>
-            </div>
-            <span className="text-stone-500"><s>70,000</s></span>
-          </div>
+          <span className="text-stone-500 pt-1">
+            <s>70,000</s>
+          </span>
         </div>
+      </div>
+    </div>
   )
 }
 
