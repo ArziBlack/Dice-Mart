147752ae1d88f9efcf208f1356698f858e1f4990
Milton Black <arziblack2@gmail.com>
2024-12-04 23:10:07 +0100

    feat: added category page...

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { ASSESSORIES, FASHION, TABLECOOKER } from "@/app/constants/images";
+import Image from "next/image";
 
 const Category = () => {
-  return <div>Category</div>;
+  const categories = [
+    { name: "Fashion", image: FASHION },
+    { name: "Funitures", image: TABLECOOKER },
+    { name: "Fashion", image: FASHION },
+    { name: "Assessories", image: ASSESSORIES },
+    { name: "Fashion", image: FASHION },
+    { name: "Assessories", image: ASSESSORIES },
+    { name: "Fashion", image: FASHION },
+    { name: "Vehicle Parts", image: TABLECOOKER },
+    { name: "Assessories", image: ASSESSORIES },
+    { name: "Funitures", image: TABLECOOKER },
+  ];
+  return (
+    <div className="px-7 flex flex-col mt-5">
+      <div className="flex items-start justify-between py-3 px-5 w-full bg-[#00468B] text-white mb-3">
+        <h2>Explore All Categories</h2>
+      </div>
+      <div className="grid grid-cols-5 gap-5 mt-5">
+        {categories.map((category, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center"
+          >
+            <div className="w-[220px] h-[220px] rounded-full">
+              <Image
+                src={category.image}
+                alt={category.name}
+                className="w-[220px] h-[220px] rounded-full"
+              />
+            </div>
+            <h2 className="text-2xl font-light mt-3 text-black-900">
+              {category.name}
+            </h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
--- app/utils/links.ts
+++ app/utils/links.ts
@@ -1,7 +1,7 @@
 export const navLinks = [
   { name: "Home", path: "/" },
   { name: "Favorites", path: "/favourite" },
-  { name: "Categories", path: "" },
+  { name: "Categories", path: "/category" },
   { name: "Gift Cards", path: "/gift_cards" },
   { name: "Contact us", path: "/contact" },
   { name: "About", path: "/about-us" },
